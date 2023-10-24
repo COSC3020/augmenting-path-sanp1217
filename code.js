9e2c4061b5f6d618cce883fdb076579b7f626864
pabs <pablosgrad12@gmail.com>
2023-10-23 21:55:14 -0600

    Adds code for augmenting path function

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
 function augmentingPath(graph, start, end) {
+    //Each node will be either true or false if it has been visited.
+    let visited = {};
+    let path = [start];
+    let currNodeQueue = [start];
+
+    while (currNodeQueue.length > 0) {
+        let current = currNodeQueue.shift();
+
+        if (current == end) {
+            return path;
+        }
+
+        visited[current] = true;
+        //Goes through each node the current node has an edge with.
+        for (let neighbor in graph[current]) {
+
+            if (visited[neighbor]) {
+                continue;
+            }
+
+            currNodeQueue.push(neighbor);
+            path = path.concat(neighbor);
+        }
+    }
+
     return [];
 }
